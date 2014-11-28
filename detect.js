3aab0a61fe4ef00d3ae3f496cd578993eda8656d
James Haley <jlhaley1@gmail.com>
2014-11-27 21:55:58 -0800

    Add detect and tests

---
+++ detect.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var curry = require('./curry');
+var isFunction = require('./is_function');
+
+module.exports = detect;
+
+function detect(condition, list) {
+  if (arguments.length > 1 && !Array.isArray(list)) {
+    throw new TypeError('Detect must be used with a list');
+  }
+
+  if (arguments.length === 1 && isFunction(condition)) {
+    return curry(detect)(condition);
+  }
+
+  var len = list.length;
+
+  for (var i = 0; i < len; i += 1) {
+    if (condition(list[i])) {
+      return list[i];
+    }
+  }
+
+  return null;
+}
---
+++ test/detect.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var detect = require('../detect');
+
+describe('detect', function() {
+  it('will detect and return the first even item', function() {
+    var list = [1, 3, 5, 6, 7, 8, 9];
+
+    expect(detect(isEven, list)).to.equal(6);
+  });
+
+  it('returns null if it cannot detect an item that meets the condition', function() {
+    var list = [1, 3, 5, 7, 9, 11];
+
+    expect(detect(isEven, list)).to.be.null();
+  });
+
+  it('curries the function for a specific condition', function() {
+    var oddList = [1, 3, 5, 7, 9, 11];
+    var evenList = [1, 3, 5, 7, 8, 9, 11];
+    var detectEven = detect(isEven);
+
+    expect(detectEven(oddList)).to.be.null();
+    expect(detectEven(evenList)).to.equal(8);
+  });
+});
+
+
+function isEven(item) {
+  return item % 2 === 0;
+}
