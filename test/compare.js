06c3ebd989874b3e5a947efa7c764141fd102b28
James Haley <jlhaley1@gmail.com>
2014-11-28 22:42:37 -0800

    Add compare and tests

---
+++ test/compare.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var chai    = require('chai');
+var expect  = chai.expect;
+var funky   = require('../funky');
+var compare = funky.compare;
+
+describe('compare', function() {
+  it("should return true when the compared object's properties are equal", function() {
+    var obj1 = { 'one': 1, 'two': 2 };
+    var obj2 = { 'zero': 0, 'two': 2 };
+
+    expect(compare('two', obj1, obj2)).to.be.true;
+  });
+
+  it("should return false when the compared object's properties are not equal", function() {
+    var obj1 = { 'one': 1, 'two': 2 };
+    var obj2 = { 'zero': 0, 'two': 3 };
+
+    expect(compare('two', obj1, obj2)).to.be.false;
+
+  });
+
+  it("should return false when both objects to not share the property", function() {
+    var obj1 = { 'one': 1, 'two': 2 };
+    var obj2 = { 'zero': 0, 'three': 3 };
+
+    expect(compare('two', obj1, obj2)).to.be.false;
+  });
+});
