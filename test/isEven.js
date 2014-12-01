abf4f0316246105539bc3766bb34c32046e4e009
James Haley <jlhaley1@gmail.com>
2014-12-01 00:10:30 -0800

    Add isEven and isOdd

---
+++ test/isEven.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var chai   = require('chai');
+var expect = chai.expect;
+var funky  = require('../funky');
+var isEven = funky.isEven;
+
+describe('isEven', function() {
+  it('should return true when the number is odd', function() {
+    expect(isEven(6)).to.be.true;
+  });
+
+  it('should return false when the number is even', function() {
+    expect(isEven(5)).to.be.false;
+  });
+});
---
+++ test/isOdd.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var chai   = require('chai');
+var expect = chai.expect;
+var funky  = require('../funky');
+var isOdd  = funky.isOdd;
+
+describe('isOdd', function() {
+  it('should return true when the number is odd', function() {
+    expect(isOdd(3)).to.be.true;
+  });
+
+  it('should return false when the number is even', function() {
+    expect(isOdd(4)).to.be.false;
+  });
+});
