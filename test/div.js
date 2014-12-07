ac9c30e41e508f5588b6e379873b6140bc5d8730
James Haley <jlhaley1@gmail.com>
2014-12-07 15:51:21 -0800

    Add currying to subtract and div plus tests

--- test/div.js
+++ test/div.js
@@ -9,4 +9,10 @@ describe('div', function() {
   it('should perform simple division', function() {
     expect(div(6, 2)).to.equal(3);
   });
+
+  it('should curry the function correctly', function() {
+    var divByTwo = div(2);
+
+    expect(divByTwo(8)).to.equal(4);
+  });
 });
--- test/subtract.js
+++ test/subtract.js
@@ -9,4 +9,10 @@ describe('subtract', function() {
   it('should perform simple subtraction', function() {
     expect(subtract(5, 2)).to.equal(3);
   });
+
+  it('should curry the function correctly', function() {
+    var subtractTwo = subtract(2);
+
+    expect(subtractTwo(8)).to.equal(6);
+  });
 });
