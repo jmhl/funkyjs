03b5582bc277afa4021ec08951b22366514726b2
James Haley <jlhaley1@gmail.com>
2014-12-06 16:17:04 -0800

    Change the way pick curries and switch arguments order

--- test/pick.js
+++ test/pick.js
@@ -8,14 +8,14 @@ var pick   = funky.pick;
 describe('pick', function() {
   it('throws an error if not passed an object', function() {
     expect(function() {
-      pick([], ['one']);
+      pick(['one'], []);
     }).to.throw(TypeError);
   });
 
   it('picks existing properties from an object', function() {
     var obj = { 'one': 1, 'two': 2, 'three': 3 };
     var expected = { 'one': 1, 'three': 3 };
-    var result = pick(obj, ['one', 'three']);
+    var result = pick(['one', 'three'], obj);
 
     expect(result).to.deep.equal(expected);
   });
@@ -23,15 +23,15 @@ describe('pick', function() {
   it('picks existing properties and ignores undefined ones', function() {
     var obj = { 'one': 1, 'two': 2, 'three': 3 };
     var expected = { 'one': 1, 'three': 3 };
-    var result = pick(obj, ['one', 'three', 'four']);
+    var result = pick(['one', 'three', 'four'], obj);
 
     expect(result).to.deep.equal(expected);
   });
 
   it('curries the object', function() {
     var obj = { 'one': 1, 'two': 2, 'three': 3 };
-    var objPick = pick(obj);
-    var result = objPick(['one', 'three']);
+    var pickProps = pick(['one', 'three']);
+    var result = pickProps(obj);
     var expected = { 'one': 1, 'three': 3 };
 
     expect(result).to.deep.equal(expected);
