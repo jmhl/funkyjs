e0d6465315d809e497957e0b1516664468fb0ea2
James Haley <jlhaley1@gmail.com>
2014-12-07 23:51:18 -0800

    Add currying to flatMap

--- test/flatMap.js
+++ test/flatMap.js
@@ -9,9 +9,20 @@ describe('map', function() {
   it('should flat map an array', function() {
     var arr = ['aa1bb', 'cc2dd', 'ee3ff'];
     var cb = function(str) { return str.split(/\d/); };
-    var res = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff'];
+
     var flatMapped = flatMap(cb, arr);
+    var res = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff'];
 
     expect(flatMapped).to.deep.equal(res);
+  });
+
+  it('should curry the function', function() {
+    var arr = ['aa1bb', 'cc2dd', 'ee3ff'];
+    var cb = function(str) { return str.split(/\d/); };
+
+    var res = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff'];
+    var flatMapSplit = flatMap(cb);
+
+    expect(flatMapSplit(arr)).to.deep.equal(res);
   });
 });
