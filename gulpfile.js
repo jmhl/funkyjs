a7c57290f8204ed7fc41fa986c47ab0eb2d66c39
James Haley <jlhaley1@gmail.com>
2014-12-26 00:02:22 -0800

    Some small cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp      = require('gulp');
 var benchmark = require('gulp-bench');
 
 gulp.task('bench', function() {
-  return gulp.src('bench/*.js', { read: false })
+  gulp.src('bench/*.js', { read: false })
     .pipe(benchmark())
     .pipe(gulp.dest('bench/report/bench.' + (new Date()).getTime() + '.json'));
 });
