afd70cfd095e2fb684d91845c0857bab18d89d93
James Haley <jlhaley1@gmail.com>
2014-12-28 18:46:47 -0800

    Add additional test cases and fixes

--- test/foldl1.js
+++ test/foldl1.js
@@ -5,6 +5,7 @@ var expect = chai.expect;
 var funky  = require('../funky');
 var foldl1 = funky.foldl1;
 var add    = funky.add;
+var div    = funky.div;
 
 describe('foldl1', function() {
   it('should perform a right fold on the array', function() {
@@ -12,5 +13,12 @@ describe('foldl1', function() {
     var folded = foldl1(add, arr);
 
     expect(folded).to.equal(6);
+  });
+
+  it('should work when the function depends on a non-zero first element', function() {
+    var arr = [8, 2, 2];
+    var folded = foldl1(div, arr);
+
+    expect(folded).to.equal(2);
   });
 });
--- test/foldr1.js
+++ test/foldr1.js
@@ -5,6 +5,7 @@ var expect = chai.expect;
 var funky  = require('../funky');
 var foldr1 = funky.foldr1;
 var add    = funky.add;
+var div    = funky.div;
 
 describe('foldr1', function() {
   it('should perform a right fold on the array', function() {
@@ -12,5 +13,12 @@ describe('foldr1', function() {
     var folded = foldr1(add, arr);
 
     expect(folded).to.equal(6);
+  });
+
+  it('should work when the function depends on a non-zero first element', function() {
+    var arr = [2, 2, 8];
+    var folded = foldr1(div, arr);
+
+    expect(folded).to.equal(2);
   });
 });
