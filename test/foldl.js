f91df826863cfd9c01a89c41c3f3f49061049abc
James Haley <jlhaley1@gmail.com>
2014-12-30 12:05:30 -0800

    Add currying to foldl

--- test/foldl.js
+++ test/foldl.js
@@ -7,17 +7,27 @@ var foldl  = funky.foldl;
 var add    = funky.add;
 
 describe('foldl', function() {
+  var arr;
+
+  before(function() {
+    arr = [1, 2, 3];
+  });
+
   it('should perform a left fold on the array', function() {
-    var arr = [1,2,3];
     var folded = foldl(add, 0, arr);
 
     expect(folded).to.equal(6);
   });
 
   it('should work for non-zero accumulators', function() {
-    var arr = [1,2,3];
     var folded = foldl(add, 10, arr);
 
     expect(folded).to.equal(16);
+  });
+
+  it('should curry the function', function() {
+    var foldLeftAdd = foldl(add);
+
+    expect(foldLeftAdd(0, arr)).to.equal(6);
   });
 });
