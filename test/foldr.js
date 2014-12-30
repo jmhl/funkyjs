4fb5f4faf37cc65480cd1caab23802169e1325e3
James Haley <jlhaley1@gmail.com>
2014-12-29 20:16:17 -0800

    Make acc the last arg in foldr

--- test/foldr.js
+++ test/foldr.js
@@ -9,14 +9,14 @@ var add    = funky.add;
 describe('foldr', function() {
   it('should perform a right fold on the array', function() {
     var arr = [1,2,3];
-    var folded = foldr(add, 0, arr);
+    var folded = foldr(add, arr, 0);
 
     expect(folded).to.equal(6);
   });
 
   it('should work for non-zero accumulators', function() {
     var arr = [1,2,3];
-    var folded = foldr(add, 10, arr);
+    var folded = foldr(add, arr, 10);
 
     expect(folded).to.equal(16);
   });
